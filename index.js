665337b5ab9ef43bfeb2783d10bfb8aff1beca9c
shoaib-mk2 <shoaibchowdhury707@gmail.com>
2023-11-08 01:34:07 +0600

    installed npm i express cors mongodb dotenv

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('SkillNest server is running')
+})
+
+app.listen(port, () => {
+    console.log(`SKillNest server is running on port: ${port}`);
+})
