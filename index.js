6152537713b7e8c7467216b65318146813a38c3d
shoaib-mk2 <shoaibchowdhury707@gmail.com>
2023-11-08 17:23:40 +0600

    Implemented a new GET endpoint '/jobById/:id' on the server that allows fetching job data from the database by its unique ID

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -43,6 +43,14 @@ async function run() {
                 query = { jobOwnerEmail: req.query.jobOwnerEmail }
             }
             const result = await jobsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // get data from database according to id
+        app.get('/jobById/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await jobsCollection.findOne(query);
             res.send(result);
         })
 
